0df1000af2b4fc737ceccb2c46a0ded59249a1d8
Simon Rowe <simon@wiremoons.com>
2021-07-05 21:59:48 +0100

    - Add SQLite DbConn type - Change to string for other SQLite record types

--- src/types.nim
+++ src/types.nim
@@ -1,10 +1,13 @@
+import db_sqlite
+
 type
   DBState* = ref object
     dbFound*: bool
     dbFullPath*: string
     dbFileName*: string
     dbFileSize*: string
-    dbRecordCount*: int
-    dbLastRecordName*: string
+    dbRecordCount*: string
+    dbLastAcronym*: string
     dbFileModTime*: string
     dbSqliteVersion*:string
+    db*: DbConn
