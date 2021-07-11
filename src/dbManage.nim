61ae2b7d9fb78b148b1ad1585bb3f70838e392a8
Simon Rowe <simon@wiremoons.com>
2021-07-11 14:15:42 +0100

    Updates to complete default output when no command line option is given: - display database modification time; path; size; records count; last acronym; sqlite version; etc - update types to include all needed structure fields - Add additional comments to document proc purpose - Add additional comments to document structure fields purpose

--- src/dbManage.nim
+++ src/dbManage.nim
@@ -7,14 +7,14 @@
 ##
 
 # import the required Nim standard library modules
-import strformat, os, db_sqlite
+import db_sqlite, strformat, strutils, times, os
 
-# import our own modules from this apps source code repo
+# import our own local modules
 import dbgUtils, types, yesno
 
 
 proc getDbConnection(dbState:DBState): bool =
-  # Attempt to connect to the SQLite database file
+  ## dbManage.getDbConnection :  Obtain a connection to the SQLite database file
   debug fmt"Attempting to open database connection to: '{dbState.dbFullPath}'"
   try:
     dbState.db = open(dbState.dbFullPath, "", "", "")
@@ -29,26 +29,47 @@ proc getDbConnection(dbState:DBState): bool =
 
 
 proc getSqliteVersion(dbState:DBState):bool =
-  # "SELECT sqlite_version()";
+  ## dbManage.getSqliteVersion : obtain the version of SQLite being used
+  ## Uses SQL: "SELECT sqlite_version()";
   dbState.dbSqliteVersion = dbState.db.getValue(sql"SELECT sqlite_version()")
   if dbState.dbSqliteVersion.len > 0: result = true else: result = false
 
 
 proc getTotalRecords(dbState:DBState):bool =
-  # "select printf('%,d', count(*)) from ACRONYMS";
+  ## dbManage.getTotalRecords : obtain the total number of acronyms records in the SQLite database
+  ## Uses SQL: "select printf('%,d', count(*)) from ACRONYMS";
   dbState.dbRecordCount = dbState.db.getValue(sql"select printf('%,d', count(*)) from ACRONYMS")
   if dbState.dbRecordCount.len > 0: result = true else: result = false
 
 
 proc getLastAcronym(dbState:DBState):bool =
-  # "SELECT Acronym FROM acronyms Order by rowid DESC LIMIT 1";
+  ## dbManage.getLastAcronym : obtain the last acronym added to the SQLite database
+  ## Uses SQL: "SELECT Acronym FROM acronyms Order by rowid DESC LIMIT 1";
   dbState.dbLastAcronym = dbState.db.getValue(sql"SELECT Acronym FROM acronyms Order by rowid DESC LIMIT 1")
   if dbState.dbLastAcronym.len > 0: result = true else: result = false
 
 
+proc getDbFileSize(dbState:DBState):bool =
+  ## dbManage.getDbFileSize : obtain the file size for the SQLite database
+  try:
+    dbState.dbFileSize = insertSep($getFileSize(dbState.dbFullPath),sep=',')
+    return true
+  except:
+    stderr.writeLine(getCurrentExceptionMsg())
+    return false
+
+proc getDbModTime(dbState:DBState):bool =
+  ## dbManage.getDbModTime : obtain the file modification time for the SQLite database
+  try:
+    let fileTime:Time = getLastModificationTime(dbState.dbFullPath)
+    dbState.dbFileModTime = $fileTime.format("ddd dd MMM yyyy HH:mm:ss")
+    return true
+  except:
+    stderr.writeLine(getCurrentExceptionMsg())
+    return false
+
 proc initDbState*(dbState:DBState) =
-  # initialise the SQLite Database structure of status. Used
-  # to track the database location, filename, records, etc
+  ## dbManage.initDbState : locate the SQLite database file and initialise the applications state structure.
 
   # Check if DB file location is given via environment variable: 'ACRODB'
   dbState.dbFullPath = getEnv("ACRODB", "")
@@ -61,8 +82,8 @@ proc initDbState*(dbState:DBState) =
     dbState.dbFileName = extractFilename(dbState.dbFullPath)
     debug "Updated DB status: " & repr(dbState)
 
+  # Check if DB file location is in same location as program executable
   if not dbState.dbFound:
-    # Check if DB file location is in same location as program executable
     dbState.dbFileName = "acronyms.db"
     dbState.dbFullPath = joinPath(getAppDir(),dbState.dbFileName)
     debug fmt"Checking for local DB file: '{dbState.dbFullPath}'" 
@@ -71,14 +92,17 @@ proc initDbState*(dbState:DBState) =
       dbState.dbFileName = extractFilename(dbState.dbFullPath)
       debug "Updated DB status: " & repr(dbState)
 
+  # If a SQLite database was found - populate the application state structure
   if dbState.dbFound:
     if getDbConnection(dbState):
       debug "Database connection OK"
       if not getSqliteVersion(dbState): writeLine(stderr, "ERROR: unable to get SQLite version")
       if not getTotalRecords(dbState): writeLine(stderr, "ERROR: unable to get total record count")
       if not getLastAcronym(dbState): writeLine(stderr, "ERROR: unable to get last acronym")
+      if not getDbFileSize(dbState): writeLine(stderr, "ERROR: unable to SQLite database file size")
+      if not getDbModTime(dbState): writeLine(stderr, "ERROR: unable to get SQLite database last modification time")
       debug "Updated DB status: " & repr(dbState)
-  else:
+  else: # no database file found
     debug "Execution of proc 'initDbState' failed to find a database file."
     dbState.dbFileName = "UNKNOWN"
     dbState.dbFullPath = "UNKNOWN"
@@ -86,7 +110,7 @@ proc initDbState*(dbState:DBState) =
 
 
 proc showDbInfo*(dbState:DBState) =
-  # display a cpy of the database state structure
+  ## dbManage.showDbInfo : display a copy of the applications state structure for the SQLite database
   echo "Database status:"
   debug "Database structure: " & repr(dbState)
   echo fmt" - Database file name: '{dbState.dbFileName}'"
@@ -101,7 +125,7 @@ proc showDbInfo*(dbState:DBState) =
 
 
 proc createNewDb*(dbState:DBState) {.noreturn} =
-    # Create a new acronyms database
+    ## dbManage.createNewDb : create a new acronyms database
     if not dbState.dbFound:
       # no database found - see if one should be created?
       if getYesNo("Would you like to create a new acronyms database?"):
--- src/types.nim
+++ src/types.nim
@@ -1,13 +1,23 @@
+##
+## SOURCE FILE: types.nim
+##
+## MIT License
+## Copyright (c) 2021 Simon Rowe
+## https://github.com/wiremoons/
+##
+
+# import the required Nim standard library modules
 import db_sqlite
 
+# types.DBState : applications state structure object
 type
   DBState* = ref object
-    dbFound*: bool
-    dbFullPath*: string
-    dbFileName*: string
-    dbFileSize*: string
-    dbRecordCount*: string
-    dbLastAcronym*: string
-    dbFileModTime*: string
-    dbSqliteVersion*:string
-    db*: DbConn
+    dbFound*: bool              # loctaed a SQLite database file path
+    dbFullPath*: string         # full path and filename for the SQLite database file
+    dbFileName*: string         # filename only for SQLite database file (ie no path)
+    dbFileSize*: string         # size in bytes as a string for the SQlite database file
+    dbFileModTime*: string      # last file modification timestamp for the SQLite database file
+    dbRecordCount*: string      # number of acronym records in the SQLite database
+    dbLastAcronym*: string      # last acronym entered into the SQLite database
+    dbSqliteVersion*:string     # The version of the SQLite being used by this application
+    db*: DbConn                 # SQLite database connection handle
